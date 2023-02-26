4fa8a3bd3518bc9b3b1919774546f0ac79398944
OSR41 <cheplygin88@gmail.com>
2023-02-26 19:17:15 +1200

    ДЗ 13.2

--- main.py
+++ main.py
@@ -15,24 +15,28 @@ class Stack:
         new_top = Node(data, next_node)
         self.top = new_top
 
+    def pop(self):
+        remove = self.top
+        self.top = self.top.next_node
+        return remove.data
+
 
 def main():
-    n1 = Node(5, None)
-    n2 = Node('a', n1)
-    print(n1.data)
-    print(n2.data)
-    print(n1)
-    print(n2.next_node)
+    stack = Stack()
+    stack.push('data1')
+    data = stack.pop()
+    print(stack.top)
+
+    print(data)
 
     stack = Stack()
     stack.push('data1')
     stack.push('data2')
-    stack.push('data3')
+    data = stack.pop()
+
     print(stack.top.data)
-    print(stack.top.next_node.data)
-    print(stack.top.next_node.next_node.data)
-    print(stack.top.next_node.next_node.next_node)
-    print(stack.top.next_node.next_node.next_node.data)
+
+    print(data)
 
 
 if __name__ == '__main__':
